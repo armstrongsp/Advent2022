a4cb0e00bb5371a36b36f60d60f281e38176462e
Scott Armstrong <scott.armstrong@contractor.crossroads.net>
2022-12-01 08:12:22 -0500

    Day 1

---
+++ Advent2022/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Advent2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1();
+        }
+
+        private static void Day1()
+        {
+            List<int> calories = new List<int>();
+            string lineText;
+            int line;
+            int total = 0;
+            using (StreamReader data = new StreamReader(AppContext.BaseDirectory + "day1.txt"))
+            {
+                lineText = data.ReadLine();
+                while (lineText != null)
+                { 
+                    if (!int.TryParse(lineText, out line))
+                    {
+                        calories.Add(total);
+                        total = 0;
+                    }
+                    else
+                    {
+                        total += line;
+                    }
+                    lineText = data.ReadLine();
+                }
+                calories.Add(total);
+            }
+            calories.Sort();
+            Console.WriteLine("Max = " + calories[calories.Count - 1]); 
+            Console.WriteLine("2nd = " + calories[calories.Count - 2]); 
+            Console.WriteLine("3rd = " + calories[calories.Count - 3]);
+            Console.WriteLine("Total Backup = " + (calories[calories.Count - 1] + calories[calories.Count - 2] + calories[calories.Count - 3]));
+        }
+    }
+}
